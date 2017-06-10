e7cbde1234adc75d2c9674248da930f5e68b0857
MasqueradeU <gigigiaru776@yahoo.co.jp>
2017-06-10 17:29:49 +0900

    C問題が数学チックすぎてわからなかった。一定の法則が掴めそうで掴めないやつ

---
+++ abc052/a.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+<<<<<<< HEAD
+
+using namespace std;
+
+=======
+ 
+using namespace std;
+ 
+>>>>>>> 517f1bc... C問題が数学チックすぎてわからなかった。一定の法則が掴めそうで掴めないやつ
+int main(){
+	int A,B,C,D;
+	cin>>A>>B>>C>>D;
+	
+	if(A*B>C*D)cout<<A*B;
+	else if(A*B<C*D)cout<<C*D;
+	else cout<<A*B;
+	
+	return 0;
+<<<<<<< HEAD
+}
+=======
+}
+>>>>>>> 517f1bc... C問題が数学チックすぎてわからなかった。一定の法則が掴めそうで掴めないやつ
---
+++ abc052/b.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<string>
+<<<<<<< HEAD
+
+using namespace std;
+
+=======
+ 
+using namespace std;
+ 
+>>>>>>> 517f1bc... C問題が数学チックすぎてわからなかった。一定の法則が掴めそうで掴めないやつ
+int main(){
+	int N;
+	string S;
+	cin>>N>>S;
+	
+	int current=0;
+	int max=0;
+	for(int i=0 ; i<S.size() ; i++){
+		if(S.substr(i,1)=="I")current+=1;
+		else current-=1;
+		if(current>max)max=current;
+	}
+	cout<<max<<"\n";
+	
+	return 0;
+<<<<<<< HEAD
+}
+=======
+}
+>>>>>>> 517f1bc... C問題が数学チックすぎてわからなかった。一定の法則が掴めそうで掴めないやつ
