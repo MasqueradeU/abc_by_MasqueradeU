663f7484ee55d0e59f827060066558bf0bcbf9fc
MasqueradeU <gigigiaru776@yahoo.co.jp>
2017-06-24 19:25:02 +0900

    templete

---
+++ templete.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+int main(){
+	int ;
+	cin>>>>;
+    vector <>();
+    for(int i=0 ; i< ; i++){
+        cin>> ;
+    }
+	
+	
+	
+	
+	return 0;
+}
